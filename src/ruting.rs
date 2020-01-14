986ce822326f323aab0bfceeb98667da48ce0cbe
Kent Daleng <kent.stefan.daleng@nav.no>
2020-01-14 12:30:02 +0100

    Legger til endepunkt for støtte av trailing / og uten

--- src/ruting.rs
+++ src/ruting.rs
@@ -4,14 +4,21 @@ use actix_web::{
 use crate::TEKSTER;
 
 
-#[get("syfotekster/internal/isAlive")]
+#[get("/syfotekster/internal/isAlive")]
 pub fn er_levende() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().finish())
 }
 
-#[get("syfotekster/internal/isReady")]
+#[get("/syfotekster/internal/isReady")]
 pub fn er_klar() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().finish())
+}
+
+#[get("/syfotekster/api/tekster")]
+pub fn tekster() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(&*TEKSTER))
 }
 
 #[get("/syfotekster/api/tekster/")]
